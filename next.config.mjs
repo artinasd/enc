8167f623873bd7448184a15c00ac12235c27be8d
artinasd <128320019+artinasd@users.noreply.github.com>
2026-09-12 18:44:25 +0330

    fix: use root asset paths for custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
-
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: isGitHubPages ? '/enc' : '',
   images: {
     unoptimized: true,
   },
